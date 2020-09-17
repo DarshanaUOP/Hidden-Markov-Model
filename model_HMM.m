176574928904757d36f9a2e651086978eec9ec08
Darshana Ariyarathna <darshana.uop@gmail.com>
2020-09-17 17:18:13 +0530

    Create model_HMM.m

---
+++ model_HMM.m
@@ -0,0 +1,19 @@
+clear;
+close all;
+
+%% initialize
+a = [
+    
+    ]; % 4x4 matrix (transmission probabilities)
+
+b = [
+    
+    ]; % 4x4 matrix (emmision probabilities)
+
+b_jk = 1;
+
+%  * initial probabilities
+%  * calculate initial probabilities?
+alpha = [0.25 0.25 0.25 0.25];    
+
+alpha = b_jk * aplha * a;
